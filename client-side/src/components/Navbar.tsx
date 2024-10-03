67bea59a520d57516dd1f137193b9bf36e0f5ddc
H3c7o4 <hectorvladitok@gmail.com>
2024-10-03 18:27:06 +0100

    Fix some issues

--- client-side/src/components/Navbar.tsx
+++ client-side/src/components/Navbar.tsx
@@ -58,7 +58,7 @@ const Navbar: React.FC = () => {
             {dropdownOpen && (
               <div className="absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5">
                 <Link
-                  to="/account"
+                  to="/profile/1"
                   className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                 >
                   Account
