d9b709173c1ec406f1ba73afc80fff5a5079a641
H3c7o4 <hectorvladitok@gmail.com>
2024-10-14 14:22:02 +0100

    Fix some issues

--- client-side/src/routes.tsx
+++ client-side/src/routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { RouterProvider, createHashRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import ProfilePage from './pages/ProfilePage';
 import NotFound from './pages/NotFound';
 import JobList from './pages/JobList';
@@ -13,7 +13,7 @@ import AccountActivation from './pages/AccountActivation';
 import SignUpSuccess from './pages/SignUpSuccess';
 
 const AppRoutes: React.FC = () => {
-    const router = createHashRouter([
+    const router = createBrowserRouter([
         {
             path: '/',
             element: <LandingPage />,
