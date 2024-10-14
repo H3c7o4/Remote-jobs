035ea3c8da049ce0c3b7a4d2df75147822337c0b
H3c7o4 <hectorvladitok@gmail.com>
2024-10-14 15:34:34 +0100

    Fix some issues with Routes URL

--- server-side/jobsite/settings.py
+++ server-side/jobsite/settings.py
@@ -181,7 +181,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://localhost:5173",
     "https://remotively.netlify.app",
-    "remotivity-bp7xvr4rl-h3c7o4s-projects.vercel.app",
+    "https://remotivity-bp7xvr4rl-h3c7o4s-projects.vercel.app",
 ]
 
 DJOSER = {
